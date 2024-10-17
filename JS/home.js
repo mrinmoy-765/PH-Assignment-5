e73f265ebf4feb956dbc41458711765223d20c39
Mrinmoy <mrinmoyd629@gmail.com>
2024-10-18 03:17:06 +0600

    three campaings functionality added

--- JS/home.js
+++ JS/home.js
@@ -13,8 +13,39 @@ document.getElementById('donateBoxBtn1')
     document.getElementById('campOneCollection').innerText = TotalCampOneCollection;
     document.getElementById('Balance').innerText = newBalance;
 
-     console.log(TotalCampOneCollection);
+    // console.log(TotalCampOneCollection);
 })
+
+document.getElementById('donateBoxBtn2')
+.addEventListener(`click`, function(event){
+    event.preventDefault();
+
+     const donateBoxTwoValue = getInputFieldValueById('donateBox2');
+     let balance = getTextFieldValueById('Balance');
+     let campTwoCollection = getTextFieldValueById('campTwoCollection');
+     let TotalCampTwoCollection = campTwoCollection += donateBoxTwoValue;
+     let newBalance = balance -= donateBoxTwoValue;
+    document.getElementById('campTwoCollection').innerText = TotalCampTwoCollection;
+    document.getElementById('Balance').innerText = newBalance;
+
+    // console.log(TotalCampOneCollection);
+})
+
+document.getElementById('donateBoxBtn3')
+.addEventListener(`click`, function(event){
+    event.preventDefault();
+
+     const donateBoxThreeValue = getInputFieldValueById('donateBox3');
+     let balance = getTextFieldValueById('Balance');
+     let campThreeCollection = getTextFieldValueById('campThreeCollection');
+     let TotalCampThreeCollection = campThreeCollection += donateBoxThreeValue;
+     let newBalance = balance -= donateBoxThreeValue;
+    document.getElementById('campThreeCollection').innerText = TotalCampThreeCollection;
+    document.getElementById('Balance').innerText = newBalance;
+
+    // console.log(TotalCampOneCollection);
+})
+
 
 
 
